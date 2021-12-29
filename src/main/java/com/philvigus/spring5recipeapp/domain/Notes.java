a1b5a090ebe310e772935c4cec44372c3ad267f4
PhilipVigus <philipvigus@yahoo.co.uk>
2021-12-29 17:14:15 +0000

    Create Notes and Recipe domain models

---
+++ src/main/java/com/philvigus/spring5recipeapp/domain/Notes.java
@@ -0,0 +1,40 @@
+package com.philvigus.spring5recipeapp.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
