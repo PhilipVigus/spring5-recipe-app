7ba2ba90ee5ee648932268e1b3c003429ff4f2e9
PhilipVigus <philipvigus@yahoo.co.uk>
2021-12-29 17:37:23 +0000

    Create UnitOfMeasure domain model and add relationship to Ingredient

--- src/main/java/com/philvigus/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/com/philvigus/spring5recipeapp/domain/Ingredient.java
@@ -1,6 +1,11 @@
 package com.philvigus.spring5recipeapp.domain;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import java.math.BigDecimal;
 
 @Entity
@@ -11,8 +16,9 @@ public class Ingredient {
 
     private String description;
     private BigDecimal amount;
-    // TODO: add
-    // private UnitOfMeasure unitOfMeasure;
+
+    @OneToOne
+    private UnitOfMeasure unitOfMeasure;
 
     @ManyToOne
     private Recipe recipe;
@@ -39,6 +45,14 @@ public class Ingredient {
 
     public void setAmount(BigDecimal amount) {
         this.amount = amount;
+    }
+
+    public UnitOfMeasure getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
     }
 
     public Recipe getRecipe() {
