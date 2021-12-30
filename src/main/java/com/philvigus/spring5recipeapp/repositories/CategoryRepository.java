f1ccac9565c1c433451594fa805dd806286992b8
PhilipVigus <philipvigus@yahoo.co.uk>
2021-12-30 15:37:09 +0000

    Add findByDescription query methods

--- src/main/java/com/philvigus/spring5recipeapp/repositories/CategoryRepository.java
+++ src/main/java/com/philvigus/spring5recipeapp/repositories/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.philvigus.spring5recipeapp.repositories;
 import com.philvigus.spring5recipeapp.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/com/philvigus/spring5recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/philvigus/spring5recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package com.philvigus.spring5recipeapp.repositories;
 import com.philvigus.spring5recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
