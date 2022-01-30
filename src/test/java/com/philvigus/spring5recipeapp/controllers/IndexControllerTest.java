4a5c84d69c20545d2901e337f75989f5358271ab
PhilipVigus <philipvigus@yahoo.co.uk>
2022-01-30 17:13:46 +0000

    Add IndexController test got getIndexPage method

---
+++ src/test/java/com/philvigus/spring5recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,40 @@
+package com.philvigus.spring5recipeapp.controllers;
+
+import com.philvigus.spring5recipeapp.services.RecipeServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+class IndexControllerTest {
+    @Mock
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController controller;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        controller = new IndexController(recipeService);
+    }
+
+    @Test
+    void getIndexPage() {
+        String viewName = controller.getIndexPage(model);
+
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
